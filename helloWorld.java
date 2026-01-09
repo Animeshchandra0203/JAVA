b5122ff1322c2270f5c849c5794789132e3f8e07
Animeshchandra0203 <animesh.chandra0203@gmail.com>
2026-01-09 19:11:22 +0530

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+class Hi{
+    public static void main(String[] args){
+          System.out.println("hello world");
+          System.out.print("my name is ");
+          System.out.print("Animesh chandra");
+          System.out.println("java is a high level, " + "object oriented, " + "general purpose programming language");
+          System.out.print("that is platform independent and is used to create application software.");
+          
+    }
+}
