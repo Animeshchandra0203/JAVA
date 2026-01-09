418def6566d8773bfaf571f933712b459205df16
Animeshchandra0203 <animesh.chandra0203@gmail.com>
2026-01-09 19:11:29 +0530

    Create helloWorldPublicClass.java

---
+++ helloWorldPublicClass.java
@@ -0,0 +1,9 @@
+public class helloWorldPublicClass {
+    public static void main(String[] args){
+          System.out.println("hello world");
+          System.out.print("my name is ");
+          System.out.print("Animesh chandra");
+    
+    }
+}
+
