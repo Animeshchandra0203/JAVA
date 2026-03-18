2fcebd461cda55bdad46413883736294a9ec7df9
Animeshchandra0203 <animesh.chandra0203@gmail.com>
2026-03-18 19:42:35 +0530

    Update JyotiMamPattern.java

--- JyotiMamPattern.java
+++ JyotiMamPattern.java
@@ -1,14 +1,11 @@
 import java.util.*;
-public class jyotiMamPattern {
+public class JyotiMamPattern {
 	public static void main(String[] args) {
 		System.out.print("enter size: ");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int a=1;
 		for(int i=1;i<=n;i++){
-			
-			for(int s=1;s<=(4/2);s++)
-			   System.out.print(" ");
 			   
 			 if(i%4 == 0){
 			 	for(int k=1;k<=4;k++){
@@ -18,8 +15,13 @@ public class jyotiMamPattern {
 			      a++;
 			 	System.out.println("");
 			 }
-			 else
+			 else{
+				for(int s=1;s<=(4/2);s++)
+			   System.out.print(" ");
+			
 			 	System.out.println((i%4)*a);
+			 }
 		}
+		sc.close();
 	}
 }
